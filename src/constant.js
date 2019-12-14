565bf8de5e2d761a76cf401ff2c584d7170f59ef
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2019-12-15 00:25:34 +0100

    add tailwind css & image
    add style css and image to pokemon card

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,22 @@
 export const POKEMON_MOCK = [
   {
+    id: 1,
+    name: "Dracolosse",
+    img:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/149.png",
+    level: 29,
+    baseAttack: 263,
+    baseDefense: 198,
+    baseStamina: 209,
+    individualAttack: 15,
+    individualDefense: 12,
+    individualStamina: 11
+  },
+  {
+    id: 2,
     name: "Aquali",
+    img:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/134.png",
     level: 28,
     baseAttack: 205,
     baseDefense: 161,
@@ -8,6 +24,19 @@ export const POKEMON_MOCK = [
     individualAttack: 14,
     individualDefense: 11,
     individualStamina: 9
+  },
+  {
+    id: 3,
+    name: "Aligateur",
+    img:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/160.png",
+    level: 32,
+    baseAttack: 205,
+    baseDefense: 188,
+    baseStamina: 198,
+    individualAttack: 15,
+    individualDefense: 10,
+    individualStamina: 15
   }
 ];
 
--- src/index.js
+++ src/index.js
@@ -33,16 +33,44 @@ function App() {
     return { cp, attack, defense, stamina };
   };
 
-  const activePokemon = POKEMON_MOCK[0];
-  const statsPokemon = pokemonStats(CP_MULTIPLIER, activePokemon);
-
   return (
     <div className="App">
-      <h1>{activePokemon.name}</h1>
-      <h2>{`Combat Point : ${statsPokemon.cp}`}</h2>
-      <h3>{`Attack : ${statsPokemon.attack}`}</h3>
-      <h3>{`Defense : ${statsPokemon.defense}`}</h3>
-      <h3>{`Stamina : ${statsPokemon.stamina}`}</h3>
+      {POKEMON_MOCK.map(pokemon => {
+        const statsPokemon = pokemonStats(CP_MULTIPLIER, pokemon);
+        return (
+          <div
+            className="max-w-md flex items-start bg-gray-300 rounded m-2 mb-4"
+            key={pokemon.id}
+          >
+            <div className="flex-shrink-0 p-2">
+              <img
+                className="center justify-center"
+                src={pokemon.img}
+                alt="Woman paying for a purchase"
+              />
+            </div>
+            <div className="flex-auto py-3 pr-3">
+              <div className="flex row items-baseline justify-between pb-2">
+                <h1 className="text-xl text-gray-900 font-medium">{pokemon.name}</h1>
+                <h2 className="text-base text-gray-800 font-medium">{`Combat Point : ${
+                  statsPokemon.cp
+                }`}</h2>
+              </div>
+              <ul className="list-none">
+                <li className="text-base text-gray-800">{`Attack : ${
+                  statsPokemon.attack
+                }`}</li>
+                <li className="text-base text-gray-800">{`Defense : ${
+                  statsPokemon.defense
+                }`}</li>
+                <li className="text-base text-gray-800">{`Stamina : ${
+                  statsPokemon.stamina
+                }`}</li>
+              </ul>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
