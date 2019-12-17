3b8fca17c3994dd4b104f8b1fcb68434ffb3585e
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2019-12-17 22:48:05 +0100

    Dps with effectiveness bonus multiplier

---
+++ src/pokemons/boss.js
@@ -0,0 +1,76 @@
+import * as TYPES from "../types"
+
+export const BOSS_MOCK = [
+  {
+    id: 129,
+    name: "Magicarpe",
+    type: [TYPES.WATER],
+    level: 41,
+    baseAttack: 29,
+    baseDefense: 85,
+    baseStamina: 700,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  },
+  {
+    id: 73,
+    name: "Tentacruel",
+    type: [TYPES.WATER, TYPES.POISON],
+    level: 41,
+    baseAttack: 166,
+    baseDefense: 209,
+    baseStamina: 2025,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  },
+  {
+    id: 196,
+    name: "Mentali",
+    type: [TYPES.PSYCHIC],
+    level: 41,
+    baseAttack: 261,
+    baseDefense: 175,
+    baseStamina: 3000,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  },
+  {
+    id: 248,
+    name: "Tyranocif",
+    type: [TYPES.DARK, TYPES.ROCK],
+    level: 41,
+    baseAttack: 251,
+    baseDefense: 207,
+    baseStamina: 7500,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  },
+  {
+    id: 384,
+    name: "Rayquaza",
+    type: [TYPES.DRAGON, TYPES.FLYING],
+    level: 41,
+    baseAttack: 284,
+    baseDefense: 170,
+    baseStamina: 12500,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  },
+  {
+    id: 150,
+    name: "MewTwo",
+    type: [TYPES.PSYCHIC],
+    level: 41,
+    baseAttack: 300,
+    baseDefense: 182,
+    baseStamina: 22500,
+    individualAttack: 15,
+    individualDefense: 15,
+    individualStamina: 0
+  }
+];
