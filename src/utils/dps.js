cf9dcb8123cf7f5002004f724b8158e602b923d6
Nicolas Vigneux <vigneuxnicolas@gmail.com>
2019-12-24 16:37:37 +0100

    test simulate battle with a while

---
+++ src/utils/dps.js
@@ -0,0 +1,43 @@
+import { getWeatherBonus } from "./weather";
+import { getMoveEffectivenessType } from "./et";
+
+const getDpsMove = (move, pokemon, opponent, weather) => {
+  const stab = pokemon.type.includes(move.type) ? 1.2 : 1;
+  const bonusWeather = getWeatherBonus(weather, move.type);
+  const effectiveness = getMoveEffectivenessType(move.type, opponent.type);
+
+  return (
+    Math.floor(
+      0.5 *
+        move.power *
+        stab *
+        effectiveness *
+        bonusWeather *
+        (pokemon.stats.attack / opponent.stats.defense)
+    ) + 1
+  );
+};
+
+export const getRealDps = (pokemon, opponent, weather) => {
+  const moveDpsQuick = getDpsMove(
+    pokemon.moves.quick,
+    pokemon,
+    opponent,
+    weather
+  );
+  const moveDpsCharged = getDpsMove(
+    pokemon.moves.charged,
+    pokemon,
+    opponent,
+    weather
+  );
+  const numberAttackRequired = Math.ceil(
+    pokemon.moves.charged.energyReq / pokemon.moves.quick.energyGen
+  );
+  const calculRealDps =
+    (moveDpsQuick * numberAttackRequired + moveDpsCharged) /
+    (pokemon.moves.quick.execTime * numberAttackRequired +
+      pokemon.moves.charged.execTime);
+
+  return Math.round(calculRealDps * 100) / 100;
+};
