f6e107bafe98fbf3e90abaf7a2283f6cc67cb71a
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-01-02 22:17:43 +0100

    [+] reducer for attack in battle

--- src/utils/battle.js
+++ src/utils/battle.js
@@ -2,62 +2,105 @@ const TIME_LIMIT = 180
 const ATK_DELAY = 700
 const DEF_DELAY = 1600
 
+const QUICK_ATK = "quick"
+const CHARGED_ATK = "charged"
+
+const battleReducer = (state, { type, attacker, timer }) => {
+  switch (type) {
+    case QUICK_ATK: {
+      return {
+        ...state,
+        oppHp: state.oppHp - attacker.moves.quick.dmg,
+        oppEnergy: state.oppEnergy + attacker.moves.quick.dmg / 2,
+        attEnergy: state.attEnergy + attacker.moves.quick.energyGen,
+        attTimeNextAtt: timer - attacker.moves.quick.execTime * 1000,
+      }
+    }
+    case CHARGED_ATK: {
+      return {
+        ...state,
+        oppHp: state.oppHp - attacker.moves.charged.dmg,
+        oppEnergy: state.oppEnergy + attacker.moves.charged.dmg / 2,
+        attEnergy: state.attEnergy - attacker.moves.charged.energyReq,
+        attTimeNextAtt: timer - attacker.moves.charged.execTime * 1000,
+      }
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${type}`)
+    }
+  }
+}
+
 export const simulateBattle = (pokemon, opponent) => {
   const timerBattle = TIME_LIMIT * 1000 // fight duration : 180000ms
-  let events = []
+  let resumeBattle = { attacker: pokemon, defender: opponent, events: [] }
   let timerRemaining = timerBattle // fight duration : 180000ms
   let stateBattle = {
     attHp: pokemon.stats.stamina,
     oppHp: opponent.stats.stamina,
     attEnergy: 0,
     oppEnergy: 0,
-    attTime: 0,
-    oppTime: 0,
+    attTimeNextAtt: 0,
+    oppTimeNextAtt: 0,
   }
 
   for (timerRemaining; timerRemaining > 0; timerRemaining -= 100) {
-    console.log("time", timerRemaining / 1000)
+    // console.log("time", timerRemaining / 1000, resumeBattle)
+
+    if (stateBattle.oppHp <= 0) {
+      console.log("Defender is KO", resumeBattle)
+      break
+    }
+
+    if (timerRemaining === 0) {
+      console.log("Timer is over", resumeBattle)
+      break
+    }
 
     // Attacker can begin launch quick attack
     if (timerRemaining === timerBattle - ATK_DELAY) {
-      stateBattle.oppHp -= pokemon.moves.quick.dmg
-      stateBattle.oppEnergy += pokemon.moves.quick.dmg / 2
-      stateBattle.attEnergy = pokemon.moves.quick.energyGen
-      stateBattle.attTime = timerRemaining - pokemon.moves.quick.execTime * 1000
+      stateBattle = battleReducer(stateBattle, {
+        type: QUICK_ATK,
+        attacker: pokemon,
+        timer: timerRemaining,
+      })
+      resumeBattle = {
+        ...resumeBattle,
+        events: [...resumeBattle.events, { stateBattle, attack: QUICK_ATK }],
+      }
     }
 
-    if (timerRemaining === stateBattle.attTime) {
+    if (timerRemaining === stateBattle.attTimeNextAtt) {
       // if Attacker can launch a charged attack
       if (pokemon.moves.charged.energyReq <= stateBattle.attEnergy) {
-        stateBattle.oppHp -= pokemon.moves.charged.dmg
-        stateBattle.oppEnergy += pokemon.moves.charged.dmg / 2
-        stateBattle.attEnergy -= pokemon.moves.charged.energyReq
-        stateBattle.attTime =
-          timerRemaining - pokemon.moves.charged.execTime * 1000
-      }
-
-      // Attacker launch quick attack
-      if (pokemon.moves.charged.energyReq > stateBattle.attEnergy) {
-        stateBattle.oppHp -= pokemon.moves.quick.dmg
-        stateBattle.oppEnergy += pokemon.moves.quick.dmg / 2
-        stateBattle.attEnergy += pokemon.moves.quick.energyGen
-        stateBattle.attTime =
-          timerRemaining - pokemon.moves.quick.execTime * 1000
+        stateBattle = battleReducer(stateBattle, {
+          type: CHARGED_ATK,
+          attacker: pokemon,
+          timer: timerRemaining,
+        })
+        resumeBattle = {
+          ...resumeBattle,
+          events: [
+            ...resumeBattle.events,
+            { stateBattle, attack: CHARGED_ATK },
+          ],
+        }
+      } else {
+        stateBattle = battleReducer(stateBattle, {
+          type: QUICK_ATK,
+          attacker: pokemon,
+          timer: timerRemaining,
+        })
+        resumeBattle = {
+          ...resumeBattle,
+          events: [...resumeBattle.events, { stateBattle, attack: QUICK_ATK }],
+        }
       }
     }
 
     // Defender can begin
     if (timerRemaining === timerBattle - DEF_DELAY) {
       console.log("Defender first attack")
-    }
-
-    if (stateBattle.oppHp <= 0) {
-      console.log("Defender is KO")
-      break
-    }
-
-    if (timerRemaining === 0) {
-      console.log("Timer is over")
     }
   }
 }
