3ccb5fc2b0bce91d37cc9283d6b882a3c54576e7
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-01-04 21:38:50 +0100

    [+] constant

--- src/constant.js
+++ src/constant.js
@@ -58,3 +58,10 @@ export const IMG_URL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
 export const IMG_FORMAT = ".png"
+
+const TIME_LIMIT = 180
+export const TIMER_BATTLE = TIME_LIMIT * 1000 // fight duration : 180000ms
+export const QUICK_ATK_ATTACKER = "Attacker Quick"
+export const CHARGED_ATK_ATTACKER = "Attacker Charged"
+export const QUICK_ATK_DEFENDER = "Defender Quick"
+export const CHARGED_ATK_DEFENDER = "Defender Charged"
