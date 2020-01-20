1c47b40eb251c44a182414a513a1a0ae82055037
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-01-20 22:02:09 +0100

    [+] id base to option pokemon

--- src/components/OptionPokemon/OptionPokemon.jsx
+++ src/components/OptionPokemon/OptionPokemon.jsx
@@ -2,15 +2,18 @@ import React from "react"
 
 import { IMG_FORMAT, IMG_URL } from "../../constants/constant"
 
-const OptionPokemon = ({ id, name }) => {
+const OptionPokemon = ({ id, name, id_base_pokemon }) => {
   return (
     <div className="flex items-center">
-      <img
-        className="h-8 mr-2"
-        src={IMG_URL + id + IMG_FORMAT}
-        alt={`${name}`}
-      />
-      <div className="text-base text-gray-900 font-medium">{name}</div>
+      <span className="mr-4 text-base text-gray-600 font-light">{`#${id_base_pokemon}`}</span>
+      <div className="flex items-center">
+        <img
+          className="h-8 mr-2"
+          src={IMG_URL + id + IMG_FORMAT}
+          alt={`${name}`}
+        />
+        <div className="text-base text-gray-900 font-medium">{name}</div>
+      </div>
     </div>
   )
 }
