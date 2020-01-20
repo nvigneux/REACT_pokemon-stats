362eb1b4f015efbef5e48737220afafc7c226360
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-01-20 21:29:44 +0100

    [+] picto types

---
+++ src/components/PictoType/PictoType.jsx
@@ -0,0 +1,52 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// Icons
+import { ReactComponent as Bug } from "../../assets/icons/bug.svg"
+import { ReactComponent as Dark } from "../../assets/icons/dark.svg"
+import { ReactComponent as Dragon } from "../../assets/icons/dragon.svg"
+import { ReactComponent as Electric } from "../../assets/icons/electric.svg"
+import { ReactComponent as Fairy } from "../../assets/icons/fairy.svg"
+import { ReactComponent as Fighting } from "../../assets/icons/fighting.svg"
+import { ReactComponent as Fire } from "../../assets/icons/fire.svg"
+import { ReactComponent as Flying } from "../../assets/icons/flying.svg"
+import { ReactComponent as Ghost } from "../../assets/icons/ghost.svg"
+import { ReactComponent as Grass } from "../../assets/icons/grass.svg"
+import { ReactComponent as Ground } from "../../assets/icons/ground.svg"
+import { ReactComponent as Ice } from "../../assets/icons/ice.svg"
+import { ReactComponent as Normal } from "../../assets/icons/normal.svg"
+import { ReactComponent as Poison } from "../../assets/icons/poison.svg"
+import { ReactComponent as Psychic } from "../../assets/icons/psychic.svg"
+import { ReactComponent as Rock } from "../../assets/icons/rock.svg"
+import { ReactComponent as Steel } from "../../assets/icons/steel.svg"
+import { ReactComponent as Water } from "../../assets/icons/water.svg"
+
+export const availablePictos = {
+  bug: <Bug />,
+  dark: <Dark />,
+  dragon: <Dragon />,
+  electric: <Electric />,
+  fairy: <Fairy />,
+  fighting: <Fighting />,
+  fire: <Fire />,
+  flying: <Flying />,
+  ghost: <Ghost />,
+  grass: <Grass />,
+  ground: <Ground />,
+  ice: <Ice />,
+  normal: <Normal />,
+  poison: <Poison />,
+  psychic: <Psychic />,
+  rock: <Rock />,
+  steel: <Steel />,
+  water: <Water />,
+}
+
+export const PictoType = ({ icon, ...props }) =>
+  React.cloneElement(availablePictos[icon], props)
+
+PictoType.propTypes = {
+  icon: PropTypes.oneOf(Object.keys(availablePictos)).isRequired,
+}
+
+export default PictoType
--- src/constants/types.js
+++ src/constants/types.js
@@ -16,3 +16,24 @@ export const PSYCHIC = "psychic"
 export const ROCK = "rock"
 export const STEEL = "steel"
 export const WATER = "water"
+
+export const TYPES_ARRAY = [
+  { id: 1, name: BUG },
+  { id: 2, name: DARK },
+  { id: 3, name: DRAGON },
+  { id: 4, name: ELECTRIC },
+  { id: 5, name: FAIRY },
+  { id: 6, name: FIGHTING },
+  { id: 7, name: FIRE },
+  { id: 8, name: FLYING },
+  { id: 9, name: GHOST },
+  { id: 10, name: GRASS },
+  { id: 11, name: GROUND },
+  { id: 15, name: ICE },
+  { id: 13, name: NORMAL },
+  { id: 14, name: POISON },
+  { id: 15, name: PSYCHIC },
+  { id: 16, name: ROCK },
+  { id: 17, name: STEEL },
+  { id: 18, name: WATER },
+]
