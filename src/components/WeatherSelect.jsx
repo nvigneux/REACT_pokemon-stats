f3efe65f89bd399eef528a1a246586ffdd0574b5
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-02 14:50:19 +0100

    [*] weather select

--- src/components/WeatherSelect.jsx
+++ src/components/WeatherSelect.jsx
@@ -1,34 +1,58 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from "react"
+import React, { cloneElement } from "react"
+import Select, { components } from "react-select"
+import OptionWeather from "./OptionWeather"
+
+const styles = {
+  singleValue: defaultStyles => ({
+    ...defaultStyles,
+    width: "92%",
+  }),
+  valueContainer: defaultStyles => ({
+    ...defaultStyles,
+    height: "42px",
+    padding: "2px 4px",
+  }),
+  container: defaultStyles => ({
+    ...defaultStyles,
+    border: "none",
+  }),
+  control: (defaultStyles, state) => ({
+    ...defaultStyles,
+    border: "none",
+    boxShadow: 0,
+  }),
+  placeholder: defaultStyles => ({
+    ...defaultStyles,
+    fontSize: "0.75rem",
+    color: "#96a4a5",
+    textTransform: "uppercase",
+  }),
+  indicatorSeparator: () => null,
+  dropdownIndicator: defaultStyles => ({
+    ...defaultStyles,
+    padding: "8px 0px",
+    width: "12px",
+  }),
+}
 
 const WeatherSelect = ({ values, activeValue, select }) => (
-  <div className="w-full px-2 mb-6">
-    <label
-      className="block uppercase tracking-wide font-semibold text-xs my-2"
-      htmlFor="weather"
-    >
-      Méteo
-    </label>
+  <div className="w-1/3 px-2 mb-6">
     <div className="relative">
-      <select
-        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        value={activeValue}
-        onChange={e => select(e.target.value)}
-        id="weather"
-      >
-        {Object.keys(values).map(item => (
-          <option key={item}>{item}</option>
-        ))}
-      </select>
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg
-          className="fill-current h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
-          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-        </svg>
-      </div>
+      <Select
+        styles={styles}
+        options={values}
+        onChange={select}
+        defaultValue={activeValue}
+        isSearchable={false}
+        formatOptionLabel={option => {
+          return cloneElement(<OptionWeather />, { key: option.label, ...option })
+        }}
+        getOptionValue={option => option.label}
+        isOptionSelected={option =>
+          activeValue ? activeValue.label === option.label : false
+        }
+      />
     </div>
   </div>
 )
