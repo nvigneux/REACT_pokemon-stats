5626602218281c1262c7218982b2d24e7b8f04b5
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-07 22:28:50 +0100

    [*] add menu icon layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import MenuIcon from "./MenuIcon"
+
 const Layout = ({ children }) => {
   return (
     <div className="flex h-full sm:items-center min-h-screen justify-center bg-pokemon pt-4 px-2">
       <div className="flex flex-col sm:self-center mobile-size rounded overflow-hidden bg-container sm:shadow-lg p-4">
         {children}
+      </div>
+      <div className="fixed h-16 w-16 rounded-full bg-button-menu bottom-0 right-0 mr-4 mb-2 shadow-lg">
+        <MenuIcon />
       </div>
     </div>
   )
