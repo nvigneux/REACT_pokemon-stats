6b565f388eae949d84caeb0432dd7181317e6a63
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-07 22:28:34 +0100

    [+] menu icon

---
+++ src/components/MenuIcon/MenuIcon.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import style from "./MenuIcon.module.css"
+
+const MenuIcon = () => {
+  return (
+    <div className={`${style.btn} ${style.notActive}`}>
+      <span />
+      <span />
+      <span />
+    </div>
+  )
+}
+
+export default MenuIcon
