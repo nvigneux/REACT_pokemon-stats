44b280282fec0cfa4a854e207199eef97c878188
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-08 22:36:19 +0100

    [*] animation menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import MenuIcon from "./MenuIcon"
+import { MenuIcon, useMenuIcon } from "./MenuIcon"
 
 const Layout = ({ children }) => {
+  const [isActive, setIsActive] = useMenuIcon()
   return (
     <div className="flex h-full sm:items-center min-h-screen justify-center bg-pokemon pt-4 px-2">
       <div className="flex flex-col sm:self-center mobile-size rounded overflow-hidden bg-container sm:shadow-lg p-4">
         {children}
       </div>
-      <div className="fixed h-16 w-16 rounded-full bg-button-menu bottom-0 right-0 mr-4 mb-2 shadow-lg">
-        <MenuIcon />
-      </div>
+      <button
+        type="button"
+        onClick={setIsActive}
+        className="fixed h-16 w-16 rounded-full bg-button-menu bottom-0 right-0 mr-4 mb-2 shadow-lg"
+      >
+        <MenuIcon isActive={isActive} />
+      </button>
     </div>
   )
 }
