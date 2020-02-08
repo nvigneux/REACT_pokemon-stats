ceee7e6633d566a2a1e76ffb34c0a144549cccbd
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-08 22:54:51 +0100

    [*] tailwind replacement

--- src/components/MenuIcon/MenuIcon.jsx
+++ src/components/MenuIcon/MenuIcon.jsx
@@ -11,10 +11,14 @@ export const useMenuIcon = () => {
 
 export const MenuIcon = ({ isActive }) => {
   return (
-    <div className={`btn ${isActive ? "active" : "notActive"}`}>
-      <span className="span" />
-      <span className="span" />
-      <span className="span" />
+    <div
+      className={`btn-menu absolute w-8 t-1/2 cursor-pointer ${
+        isActive ? "active" : "notActive"
+      }`}
+    >
+      <span className="menu-bar block w-full rounded-sm h-1 bg-white transition-all duration-300 relative" />
+      <span className="menu-bar block w-full rounded-sm h-1 bg-white transition-all duration-300 relative" />
+      <span className="menu-bar block w-full rounded-sm h-1 bg-white transition-all duration-300 relative" />
     </div>
   )
 }
