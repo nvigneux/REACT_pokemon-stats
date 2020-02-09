0cc2634be6f78fd316c23c1ebf42d0b15da94e54
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-09 19:34:04 +0100

    [*] navigation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,23 +1,62 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { NavLink } from "react-router-dom"
+
+import Popin, { usePopin } from "./Popin/Popin"
 
 import { MenuIcon, useMenuIcon } from "./MenuIcon"
 
 const Layout = ({ children }) => {
   const [isActive, setIsActive] = useMenuIcon()
+  const [isDisplayed, toggle] = usePopin()
+
+  const handleNavigation = () => {
+    setIsActive()
+    toggle()
+  }
+
   return (
     <div className="flex h-full sm:items-center min-h-screen justify-center bg-pokemon pt-4 px-2">
       <div className="flex flex-col sm:self-center mobile-size rounded overflow-hidden bg-container sm:shadow-lg p-4">
         {children}
+        <button
+          type="button"
+          onClick={handleNavigation}
+          style={{ zIndex: 11, height: "3.5rem", width: "3.5rem" }}
+          className="fixed rounded-full bg-button-menu bottom-0 right-0 mr-4 mb-2 shadow-lg"
+        >
+          <MenuIcon isActive={isActive} />
+        </button>
+        <Popin isDisplayed={isDisplayed} onClose={handleNavigation}>
+          <>
+            <NavLink
+              activeClassName="active-menu"
+              onClick={handleNavigation}
+              to="/"
+              exact
+              className="color-menu-pokemon text-base uppercase px-1 mr-2 mb-6 hover:text-green-100"
+            >
+              Pokédex
+            </NavLink>
+            <NavLink
+              activeClassName="active-menu"
+              onClick={handleNavigation}
+              to="/boss"
+              className="color-menu-pokemon text-base uppercase px-1 mr-2 mb-6 hover:text-green-100"
+            >
+              Ajouter un Boss
+            </NavLink>
+            <NavLink
+              activeClassName="active-menu"
+              onClick={handleNavigation}
+              to="/pokedex"
+              className="color-menu-pokemon text-base uppercase px-1 mr-2 mb-6 hover:text-green-100"
+            >
+              Ajouter un Pokémon
+            </NavLink>
+          </>
+        </Popin>
       </div>
-      <button
-        type="button"
-        onClick={setIsActive}
-        style={{ height: "3.5rem", width: "3.5rem" }}
-        className="fixed rounded-full bg-button-menu bottom-0 right-0 mr-4 mb-2 shadow-lg"
-      >
-        <MenuIcon isActive={isActive} />
-      </button>
     </div>
   )
 }
