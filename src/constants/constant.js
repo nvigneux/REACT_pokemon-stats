029fc7cdd93987e11d7923c51ebd03fa11c2fe81
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-12 19:46:32 +0100

    [*]change icons to types folder

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -5,7 +5,7 @@ export const IMG_FORMAT = ".png"
 export const IMG_URL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 
-export const API_URL = "http://localhost:1337"
+export const API_URL = "http://192.168.0.16:1337"
 
 export const API_POKEMONS = `${API_URL}/pokemons`
 export const API_POKEDEXES = `${API_URL}/pokedexes`
