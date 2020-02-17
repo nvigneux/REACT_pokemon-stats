6eba2fcb89d8a6802ad9dbbbc2380291df3df8a3
VIGNEUX Nicolas <vigneuxnicolas@gmail.com>
2020-02-17 19:25:20 +0100

    [*] button click

--- src/components/organisms/Pokedexes/Pokedexes.jsx
+++ src/components/organisms/Pokedexes/Pokedexes.jsx
@@ -2,16 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import PokedexCard from "../../molecules/PokedexCard"
-
+// TODO make delete pokedex
 const Pokedexes = ({ pokedexes, click }) =>
   pokedexes.map(pokedex => {
     return (
-      <div
-        key={pokedex.id}
-        className="w-1/3 flex flex-col flex-wrap"
-        onClick={() => click(pokedex.id)}
-      >
-        <PokedexCard pokedex={pokedex} />
+      <div key={pokedex.id} className="w-1/3 flex flex-col flex-wrap">
+        <button onClick={() => click(pokedex.id)} type="button">
+          <PokedexCard pokedex={pokedex} />
+        </button>
       </div>
     )
   })
